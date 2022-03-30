bc0a74c485a8fbdb550c92d343a7f7e0a4f63964
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-30 09:06:57 +0530

    rollback script added

--- rollback.sh
+++ rollback.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
+echo ######## apt update ##########
 apt update -y
+echo ######## apt git install ##########
 apt install git -y
+echo ######## git version ##########
 git --version
+echo ######## git clone ##########
 git clone https://github.com/shrikant91/rollback_test.git
+echo ######## change directory rollback_test ##########
 cd rollback_test
 ls -lrt
 pwd
-echo ## Branch Name##
+echo ######## Branch Name ##########
 git branch
