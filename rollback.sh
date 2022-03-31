9ed59386e859c34084deda39bf75840bdad24f7b
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 16:52:31 +0530

    config changes1

--- rollback.sh
+++ rollback.sh
@@ -10,6 +10,6 @@ git config --global user.email "shreeda91@gmail.com"
 git config --global user.name "shrikant91"
 echo $1
 git revert $1
-git push https://shrikant91:shree_12345@github.com/shrikant91/rollback_test.git --all
+git push https://shrikant91:Shreeda123456789@github.com/shrikant91/rollback_test.git --all
 cd data     
 ls -lrt
