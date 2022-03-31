6fe20a8715d71f61ef484e99ce2337b5fafd57a8
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 19:12:27 +0530

    config change

--- rollback.sh
+++ rollback.sh
@@ -14,13 +14,13 @@ git config --global user.email "shreeda91@gmail.com"
 git config --global user.name "shrikant91"
 #git fetch
 echo $1
-git revert $1 && git push --force
+git revert $1 
+git remote set-url origin https://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@github.com/shrikant91/rollback_test.git
+git push https://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@github.com/shrikant91/rollback_test.git --force
 echo "push force ############"
 #git push --set-upstream origin master
 #git push --force
 echo "######## PWD ##########"
 pwd
-#git remote set-url origin https://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@github.com/shrikant91/rollback_test.git
-#git push https://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@github.com/shrikant91/rollback_test.git --force
 cd data     
 ls -lrt
