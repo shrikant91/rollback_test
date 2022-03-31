dc0b31777c3bacc80692d40b35fba64f90c5dff5
shrikant91 <shreeda91@gmail.com>
2022-03-31 23:09:50 +0530

    config change

--- rollback.sh
+++ rollback.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ######## git clone ##########
 cd /tmp/
+
 git clone https://shrikant91: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@github.com/shrikant91/rollback_test.git
 ######## change directory rollback_test ##########
 cd /tmp/rollback_test/
+git config credential.helper store
 git remote set-url origin https://shrikant91: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@github.com/shrikant91/rollback_test.git
 #cd rollback_test
 pwd 
@@ -17,7 +19,7 @@ git config --global user.name "shrikant91"
 #git fetch
 echo $1
 git revert $1 
-git remote set-url origin https://shrikant91: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@github.com/shrikant91/rollback_test.git
+#git remote set-url origin https://shrikant91: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@github.com/shrikant91/rollback_test.git
 git push origin --force
 echo "push force ############"
 #git push --set-upstream origin master
