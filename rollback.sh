981d58b35faf7a5da021bf6e7d02d1e2566707bb
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 15:45:32 +0530

    script changes done

--- rollback.sh
+++ rollback.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-echo -e ######## apt update ##########
-apt update -y
-echo -e ######## apt git install ##########
-apt install git -y
-######## git version ##########
-git --version
 ######## git clone ##########
 git clone https://github.com/shrikant91/rollback_test.git
 ######## change directory rollback_test ##########
 cd rollback_test
-ls -lrt
 pwd ######## PWD ##########
 git branch ######## Branch Name ##########
 git checkout master
-git config --global user.email "shreeda91@gmail.com"
-git config --global user.name "shrikant91"
 echo $1
 git revert $1
 git push --force
 cd data     
-ls -lrt
-git pull origin master
 ls -lrt
