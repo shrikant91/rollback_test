70df91f09bd24778f9ea18752d0cc3324c8b9f90
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 14:09:39 +0530

    changes done

--- rollback.sh
+++ rollback.sh
@@ -6,7 +6,7 @@ apt install git -y
 ######## git version ##########
 git --version
 ######## git clone ##########
-git clone https://"'shrikant91':'Shreeda#4321'"@github.com/shrikant91/rollback_test.git
+git clone https://"shrikant91:Shreeda#4321"@github.com/shrikant91/rollback_test.git
 ######## change directory rollback_test ##########
 cd rollback_test
 ls -lrt
