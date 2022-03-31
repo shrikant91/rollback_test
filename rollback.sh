28bde06b80127a32a847ef36134cb490d4caa0dc
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 16:09:40 +0530

    config changes

--- rollback.sh
+++ rollback.sh
@@ -8,7 +8,7 @@ git branch ######## Branch Name ##########
 git checkout master
 git config --global user.email "shreeda91@gmail.com"
 git config --global user.name "shrikant91"
-git remote add https://`shreeda91@gmail.com:Shreeda#4321`@github.com/shrikant91/rollback_test.git
+git remote add https://'shreeda91@gmail.com':'Shreeda#4321'@github.com/shrikant91/rollback_test.git
 echo $1
 git revert $1
 git push --force
