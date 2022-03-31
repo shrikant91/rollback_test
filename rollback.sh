7c0809b3ecb5df819516b3be9a9f9554e2645e13
Shrikant Mohinkar <Shrikant.Mohinkar@datametica.com>
2022-03-31 18:10:18 +0530

    config change

--- rollback.sh
+++ rollback.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 ######## git clone ##########
-git clone https://github.com/shrikant91/rollback_test.git
+#git clone https://github.com/shrikant91/rollback_test.git
 ######## change directory rollback_test ##########
-cd rollback_test
+#cd rollback_test
 pwd 
 git branch ######## Branch Name ##########
 git checkout master
-git config --global user.email "shreeda91@gmail.com"
-git config --global user.name "shrikant91"
-git fetch
+#git config --global user.email "shreeda91@gmail.com"
+#git config --global user.name "shrikant91"
+#git fetch
 echo $1
 git revert $1
 echo "push force ############"
 git push --force
 echo "######## PWD ##########"
 pwd
-git remote set-url origin https://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@github.com/shrikant91/rollback_test.git
-git push https://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@github.com/shrikant91/rollback_test.git --force
+#git remote set-url origin https://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@github.com/shrikant91/rollback_test.git
+#git push https://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@github.com/shrikant91/rollback_test.git --force
 cd data     
 ls -lrt
