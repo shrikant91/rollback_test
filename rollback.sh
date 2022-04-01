de9d245411501d256f17b479676bf1c3821690e6
shrikant91 <shreeda91@gmail.com>
2022-04-01 11:47:25 +0530

    config change

--- rollback.sh
+++ rollback.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ######## git clone ##########
-cd /tmp/
 
+mkdir /test_repo/
+chmod 755 -R /test_repo/
+cd /test_repo/
 git clone https://shrikant91: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@github.com/shrikant91/rollback_test.git
 ######## change directory rollback_test ##########
-cd /tmp/rollback_test/
+cd /test_repo/rollback_test/
 #git config credential.helper store
 #git remote set-url origin https://shrikant91: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@github.com/shrikant91/rollback_test.git
 #cd rollback_test
@@ -13,7 +15,7 @@ git config --global --unset-all credential.helper
 git config --system --unset-all credential.helper
 pwd 
 ls -lrt
-cd /tmp/rollback_test/
+cd /test_repo/rollback_test/
 git branch 
 git checkout master
 git pull origin master
@@ -21,16 +23,14 @@ git config --global credential.helper store
 git config --global user.email "shreeda91@gmail.com"
 git config --global user.name "shrikant91"
 git config --global user.password "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"
-git push https://`shrikant91`:`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`@github.com/shrikant91/rollback_test.git
-
+git push https://shrikant91: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@github.com/shrikant91/rollback_test.git
+cd /test_repo/rollback_test/
+git config --list 
 #git fetch
-cd /tmp/rollback_test/
 echo $1
 git revert -m 1 $1
-cd /tmp/rollback_test/
-git config --list 
-cd /tmp/rollback_test/
 echo "push force ############"
+
 git push --force
 echo "######## PWD ##########"
 pwd
