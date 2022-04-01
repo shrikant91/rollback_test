39d7d39e57bdc7d048bf54e61bf46509ec6b945d
shrikant91 <shreeda91@gmail.com>
2022-04-01 10:48:03 +0530

    config change

--- rollback.sh
+++ rollback.sh
@@ -18,11 +18,14 @@ git config --global user.email "shreeda91@gmail.com"
 git config --global user.name "shrikant91"
 git config --global user.password "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"
 #git fetch
+cd /tmp/rollback_test/
 echo $1
 git revert $1
 #git config --system --unset credential.helper
 #git remote set-url origin https://shrikant91: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@github.com/shrikant91/rollback_test.git
+cd /tmp/rollback_test/
 git config --list 
+cd /tmp/rollback_test/
 git push --force
 echo "push force ############"
 #git push --set-upstream origin master
